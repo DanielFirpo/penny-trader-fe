c5932118e273529abe7042f663ed334cf9a07d7d
Turtled <trtld2@gmail.com>
2020-08-28 17:38:13 -0700

    Added back image uploading to add coin page

--- pennytrader/src/components/pages/Admin/AddProduct.js
+++ pennytrader/src/components/pages/Admin/AddProduct.js
@@ -8,10 +8,17 @@ function AddProduct(props) {
 
     const [error, setError] = useState("");
     const [name, setName] = useState("");
+
     //for the server
-    const [imageBlob, setImageBlob] = useState("");
+    const [frontImageBlob, setFrontImageBlob] = useState("");
     //for preview in browser
-    const [imageBase64, setImageBase64] = useState("");
+    const [frontImageBase64, setFrontImageBase64] = useState("");
+
+    //for the server
+    const [backImageBlob, setBackImageBlob] = useState("");
+    //for preview in browser
+    const [backImageBase64, setBackImageBase64] = useState("");
+
     const [camara, setCamara] = useState(true);//camara facing
     const [year, setYear] = useState(19);
     const [price, setPrice] = useState();
@@ -65,6 +72,30 @@ function AddProduct(props) {
         return zeros + (parseInt(str.substring(zeros.length - 1)) + 1).toString();
     }
 
+
+    //convert base64 data from react-webcam to a Blob so it can be uploaded to server
+
+    function base64toBlob(base64Data, contentType) {
+        contentType = contentType || '';
+        var sliceSize = 1024;
+        var byteCharacters = atob(base64Data);
+        var bytesLength = byteCharacters.length;
+        var slicesCount = Math.ceil(bytesLength / sliceSize);
+        var byteArrays = new Array(slicesCount);
+
+        for (var sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
+            var begin = sliceIndex * sliceSize;
+            var end = Math.min(begin + sliceSize, bytesLength);
+
+            var bytes = new Array(end - begin);
+            for (var offset = begin, i = 0; offset < end; ++i, ++offset) {
+                bytes[i] = byteCharacters[offset].charCodeAt(0);
+            }
+            byteArrays[sliceIndex] = new Uint8Array(bytes);
+        }
+        return new Blob(byteArrays, { type: contentType });
+    };
+
     return (
         <div id="add-product" className="page-height page-padding">
 
@@ -73,41 +104,31 @@ function AddProduct(props) {
             <form id="add-product-form" name="login" method="POST" encType="multipart/form-data">
                 {/* <input type="file" name="imageName" placeholder="Coin Image" className="add-coin-form-input" id="add-coin-form-image" onChange={(e) => { setImageName(e.target.value) }}></input> */}
                 <p className="coin-input-title">Image (optional)</p>
-                <Webcam audio={false} screenshotFormat="image/png" screenshotQuality={1} videoConstraints={{width: 720, height: 720, facingMode: camara ? "user" : {exact: "environment"} }} ref={webcam} />
-                <img src={imageBase64}></img>
+                <Webcam audio={false} screenshotFormat="image/png" screenshotQuality={1} videoConstraints={{ width: 720, height: 720, facingMode: camara ? "user" : { exact: "environment" } }} ref={webcam} />
+                <br></br>
                 <button onClick={(e) => {
                     e.preventDefault();
 
-                    //convert base64 data from react-webcam to a Blob so it can be uploaded to server
+                    setFrontImageBase64(webcam.current.getScreenshot({ width: 720, height: 720 }));
+                    setFrontImageBlob(base64toBlob(webcam.current.getScreenshot({ width: 720, height: 720 }).split(",")[1], "image/png"));
 
-                    function base64toBlob(base64Data, contentType) {
-                        contentType = contentType || '';
-                        var sliceSize = 1024;
-                        var byteCharacters = atob(base64Data);
-                        var bytesLength = byteCharacters.length;
-                        var slicesCount = Math.ceil(bytesLength / sliceSize);
-                        var byteArrays = new Array(slicesCount);
-                    
-                        for (var sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
-                            var begin = sliceIndex * sliceSize;
-                            var end = Math.min(begin + sliceSize, bytesLength);
-                    
-                            var bytes = new Array(end - begin);
-                            for (var offset = begin, i = 0; offset < end; ++i, ++offset) {
-                                bytes[i] = byteCharacters[offset].charCodeAt(0);
-                            }
-                            byteArrays[sliceIndex] = new Uint8Array(bytes);
-                        }
-                        return new Blob(byteArrays, { type: contentType });
-                    };
-                    
-                    setImageBase64(webcam.current.getScreenshot({width: 720, height: 720}));
-                    setImageBlob(base64toBlob(webcam.current.getScreenshot({width: 720, height: 720}).split(",")[1], "image/png"));
-                }}>Take Image</button>
+                }}>Take Front Image</button>
+                <button onClick={(e) => {
+                    e.preventDefault();
+
+                    setBackImageBase64(webcam.current.getScreenshot({ width: 720, height: 720 }));
+                    setBackImageBlob(base64toBlob(webcam.current.getScreenshot({ width: 720, height: 720 }).split(",")[1], "image/png"));
+
+                }}>Take Back Image</button>
                 <button onClick={(e) => {
                     e.preventDefault();
                     setCamara(!camara);
                 }}>Switch Camara (current: {camara ? "user" : "environment"})</button>
+
+                <p className="coin-input-title">Front Image</p>
+                <img src={frontImageBase64}></img>
+                <p className="coin-input-title">Back Image</p>
+                <img src={backImageBase64}></img>
                 {/* <input type="file" ref={imageInput} name="file" onChange={(e) => { console.log(e.target.files[0]); console.log(webcam.current.getScreenshot()); setImage(e.target.files[0]);}}/> */}
                 <p className="coin-input-title">Name</p>
                 <input type="text" value={name} name="name" placeholder="Coin Name" className="add-coin-form-input" id="add-coin-form-name" onChange={(e) => { setName(e.target.value) }}></input>
@@ -117,56 +138,59 @@ function AddProduct(props) {
                 <input type="number" value={price} name="price" placeholder="Price of coin in dollars (e.g. 3.25)" className="add-coin-form-input" id="add-coin-form-price" onChange={(e) => { setPrice(e.target.value) }}></input>
                 <p className="coin-input-title">Description (optional)</p>
                 <input type="text" value={description} name="description" placeholder="Description of coin" className="add-coin-form-input" id="add-coin-form-description" onChange={(e) => { setDescription(e.target.value) }}></input>
-                
+
                 <p className="coin-input-title">Manufacturer</p>
                 <label>
                     P
-                    <input type="checkbox" name="p" checked={manufacturer === 0} onChange={() => { setManufacturer(0) }} /><br/>
+                    <input type="checkbox" name="p" checked={manufacturer === 0} onChange={() => { setManufacturer(0) }} /><br />
                 </label>
                 <label>
                     S
-                    <input type="checkbox" name="s" checked={manufacturer === 1} onChange={() => { setManufacturer(1) }} /><br/>
+                    <input type="checkbox" name="s" checked={manufacturer === 1} onChange={() => { setManufacturer(1) }} /><br />
                 </label>
                 <label>
                     D
-                    <input type="checkbox" name="d" checked={manufacturer === 2} onChange={() => { setManufacturer(2) }} /><br/>
+                    <input type="checkbox" name="d" checked={manufacturer === 2} onChange={() => { setManufacturer(2) }} /><br />
                 </label>
 
                 <p className="coin-input-title">Rating</p>
                 <label>
                     Poor
-                    <input type="checkbox" name="poor" checked={rating === 0} onChange={() => { setRating(0) }} /><br/>
+                    <input type="checkbox" name="poor" checked={rating === 0} onChange={() => { setRating(0) }} /><br />
                 </label>
                 <label>
                     Average
-                    <input type="checkbox" name="average" checked={rating === 1} onChange={() => { setRating(1) }} /><br/>
+                    <input type="checkbox" name="average" checked={rating === 1} onChange={() => { setRating(1) }} /><br />
                 </label>
                 <label>
                     Great
-                    <input type="checkbox" name="great" checked={rating === 2} onChange={() => { setRating(2) }} /><br/>
+                    <input type="checkbox" name="great" checked={rating === 2} onChange={() => { setRating(2) }} /><br />
                 </label>
                 <label>
                     Excellent
                     <input type="checkbox" name="excellent" checked={rating === 3} onChange={() => { setRating(3) }} />
                 </label>
-                
+
                 <p className="coin-input-title">Status</p>
                 <label>
                     Listed
-                    <input type="checkbox" name="listed" checked={status === 1} onChange={() => { setStatus(1) }} /><br/>
+                    <input type="checkbox" name="listed" checked={status === 1} onChange={() => { setStatus(1) }} /><br />
                 </label>
                 <label>
                     Unlisted
-                    <input type="checkbox" name="unlisted" checked={status === 0} onChange={() => { setStatus(0) }} /><br/>
+                    <input type="checkbox" name="unlisted" checked={status === 0} onChange={() => { setStatus(0) }} /><br />
                 </label>
                 <label>
                     Sold
-                    <input type="checkbox" name="sold" checked={status === 2} onChange={() => { setStatus(2) }} /><br/>
+                    <input type="checkbox" name="sold" checked={status === 2} onChange={() => { setStatus(2) }} /><br />
                 </label>
                 <button type="submit" onClick={(e) => {
                     e.preventDefault();
                     let formData = new FormData();
-                    formData.append('file', imageBlob);
+                    console.log("SENDING BLOBS:")
+                    console.log([frontImageBlob, backImageBlob]);
+                    formData.append('images', frontImageBlob);
+                    formData.append('images', backImageBlob);
                     formData.append('name', name);
                     formData.append('year', year);
                     formData.append('price', price);
