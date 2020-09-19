bb3f1fa935d4ce6e3f0aafb104b3e85ab3ea86d1
Turtled <trtld2@gmail.com>
2020-09-18 23:53:14 -0700

    Added image slideshow to items to view front/back of coins

--- pennytrader/src/components/Item.js
+++ pennytrader/src/components/Item.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from "react-redux"
 import { setToast, setImage } from "../actions/actions"
 
@@ -6,14 +6,20 @@ import addtocart from "../images/addtocart.svg"
 
 function Item(props) {
 
+    const [imageSelection, setImageSelection] = useState(props.item.front_image_name);
+
     return (
         <div className="item-container">
             <div className="item">
-                <img className="item-image" src={props.item.image_name == "no-image.png" ? process.env.REACT_APP_API_URL + "images/products/no-image.png" : props.item.image_name} onClick={(e) => {
+                <img className="item-image" src={imageSelection == "no-image.png" ? process.env.REACT_APP_API_URL + "images/products/no-image.png" : imageSelection} onClick={(e) => {
                     if (props.item.image_name != "no-image.png") {
                         props.setImage(props.item.image_name)
                     }
                 }}></img>
+                <div style={{ display: "flex", justifyContent: "space-between" }}>
+                    <div style={{ fontSize: "20px", cursor: "pointer" }} onClick={() => { setImageSelection(props.item.front_image_name)}}>&#60;</div>
+                    <div style={{ fontSize: "20px", cursor: "pointer" }} onClick={() => { setImageSelection(props.item.back_image_name)}}>&gt;</div>
+                </div>
                 <p className="item-title">{props.item.year} Penny - {props.item.name}</p>
                 <p className="item-description" title={props.item.description}>{props.item.description.length > 140 ? props.item.description.substring(0, 140) + "..." : props.item.description}</p>
                 <div className="item-price-and-add">
